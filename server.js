362fc7ed19ec93510e315422f57cd58bfc05a836
petemaynard <xerxespete@gmail.com>
2024-02-21 00:46:19 -0600

    Adding something heroku needs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const uuid = require('./helpers/uuid');
 
 const notesData = require('./db/db.json')
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
